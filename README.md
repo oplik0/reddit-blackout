af43b609d214c98f71ee01cbea34779c0e45c7cc
oplik0 <opliko.reg@protonmail.com>
2023-06-11 15:22:35 +0200

    feat: blackout bot

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# reddit-blackout
+# Reddit Blackout
+Set your subreddits to private for 48 hours
+
+## What is this doing
+
+This repository contains a single simple python script (`wipe_reddit_account.py`) and a single GitHub Actions workflow (`.github/workflows/wipe_reddit_account.yml`). I recommend you read them yourself before giving this access to your account. They should hopefully be simple and well-commented enough to understand even without any programming experience.
+
+The `pyproject.toml` and `poetry.lock` files are from a tool called [Poetry](https://python-poetry.org/) which is a package manager for Python that I prefer over using `pip` directly. The workflow installs and uses poetry already, but if you want to develop it locally without Poetry, there is currently just a single dependency (`praw`), so you can just install it by running `pip install praw` instead.
+
+## Usage
+
+1. Use this repository as a template or fork it (note that forks can't be set to private)
+
+![fork button location](https://user-images.githubusercontent.com/25460763/183402131-46c4955f-9545-4ca5-8c9c-da8f860075a5.png)
+
+2. Create a new Reddit app:
+    1. Go to https://www.reddit.com/prefs/apps/
+    2. Click the `are you a developer? create an app...` button at the bottom of the page
+    3. Create some name (for example `blackout`. It can't contain `reddit` though)
+    4. Select `script` as the application type
+    5. Write some URL in `redirect_uri` - for example `http://localhost` or even something like `http://invalid-uri`. It won't be useful for this application, but Reddit requires that this field contains an URL.
+    6. Other fields are optional, so you can just create the app now
+    
+    ![filled app creation form](https://user-images.githubusercontent.com/25460763/183403287-76139f11-1e2a-4100-ae8f-0e2396e3459b.png)
+3. Save two pieces of information from your created app (or just leave the tab open) - client id and secret:
+
+![client id and client secret locations](https://user-images.githubusercontent.com/25460763/183404430-656f88c5-e028-4081-b9d5-a7d7473760da.png)
+
+4. Open your forked repository in another tab and go to Settings, then Security>Secrets>Actions. You'll need to create a total of 4 repository secrets. Repository secrets are saved in encrypted form and can't be retrieved directly - they will even be hidden from actions logs (but if you give them to a malicious script in an action it can still send them somewhere, so beware):
+    - `REDDIT_CLIENT_ID` - the ID of the app from earlier
+    - `REDDIT_CLIENT_SECRET` - the app secret from earlier
+    - `REDDIT_USERNAME` - your username
+    - `REDDIT_PASSWORD` - your password; Unfortunately this authentication flow requires it. For hosted applications normal Oauth2 flow would be better, but as this repository is meant to be copied and used for a single account this is the least bad option.
+    - `REDDIT_USER_AGENT` (optional) - how the application will identify itself to Reddit. Defaults to "Blackout Bot"
+    - `SUBREDDIT_BLACKLIST` (optional) - comma separated list of subreddits excluded from being privated. Takes precedence over whitelist.
+    - `SUBREDDIT_WHITELIST` (optional) - comma separated list of subreddits. If set, only the included subreddits will be privated
+    - `SUBREDDIT_DESCRIPTION` (optional) - custom text to change the subreddit description to (inserted after the subreddit name)
+> **Warning**
+> Subreddit names will be visible in backups, so if you don't want them to be public ensure your repository is private.
+
+5. Ensure you have workflows enabled in the repository by going to the Actions tab on the top and selecting `I understand my workflows, go ahead and enable them`:
+
+![Actions warning](https://user-images.githubusercontent.com/25460763/183405553-1ce872f0-7790-466a-a115-7e3f4bdcf0dc.png)
+
+6. And it'd done - the workflow will now run at 00:00 UTC the 12th of July. You can also trigger it manually by going to Actions, selecting `.github/workflows/blackout.yml` and using the `Run workflow` button:
+
+![Running the workflow manually](https://user-images.githubusercontent.com/25460763/183406938-af2f4c77-9f8b-44bb-bf15-6943e120d1e5.png)
+
+## How to change the schedule
+
+The workflow runs using two `cron` triggers in the GitHub Actions workflow. You can easily modify the current schedule or even add more cron expressions. Just edit this part of the file (you can do this easily in the GitHub web UI):
+```yaml
+on:
+  schedule:
+    - cron: "0 0 12 06 *"
+    - cron: "0 0 14 06 *"
+```
+
+If you aren't familiar with cron's notation (and even if you are), you can use a tool like [Crontab.guru](https://crontab.guru/) to create the expression.
+
+If you want an indefinite 
---
+++ blackout.py
@@ -0,0 +1,135 @@
+"""A simple script for privating a subreddit."""
+
+# imports some built in packages:
+# os.environ is used to access environmental variables, where we will pass our secrets and variables
+from os import environ
+
+# os.path.exists is used to check if a file exists, which we will use for mode selection
+from os.path import exists
+
+# a module for reading and writing Comma Separated Value (CSV) files - basically, one of the simplest formats for storing data
+import csv
+
+# regular expression module - here just used to split a string into a list with whitespace removed
+from re import split
+
+# reddit API wrapper, basically makes connecting to reddit really simple and more readable
+# without this one would need to write a lot of code to even log in
+import praw
+
+# create a Reddit instance using environment variables for authentication (set in workflow file)
+# this will be used to interact with Reddit API
+reddit = praw.Reddit(
+    # this is the ID of the app we created in the Reddit developer portal
+    client_id=environ.get('REDDIT_CLIENT_ID'),
+    # this is the secret of the app we created in the Reddit developer portal
+    client_secret=environ.get('REDDIT_CLIENT_SECRET'),
+    # this is the username of the account we want to delete all content from
+    # should be yours otherwise it probably won't work :)
+    username=environ.get('REDDIT_USERNAME'),
+    # this is the password of the account we want to delete all content from
+    # unfortunate it is necessary for this authentication method
+    password=environ.get('REDDIT_PASSWORD'),
+    # Reddit Data API terms requires not masking the user agent - but I want this to be configurable in case they find this script and decide to block it
+    user_agent=environ.get('REDDIT_USER_AGENT') or "Blackout Bot",
+)
+
+def selected_subreddits():
+    """Generator that returns all subreddits to be privated."""
+    all_moderated = reddit.user.moderator_subreddits()
+    # support both blacklisting and whitelisting - note that if both are set, exclusions takes precedence
+    excluded = split(r"\s*,\s*", environ.get('SUBREDDIT_BLACKLIST') or "") or []
+    included = split(r"\s*,\s*", environ.get('SUBREDDIT_WHITELIST') or "") or []
+
+    for subreddit in all_moderated:
+        if subreddit.display_name in excluded:
+            continue
+        if not included or subreddit.display_name in included:
+            print(f"Including {subreddit.display_name}")
+            yield subreddit
+
+def restore_subreddit(name, description, type):
+    """Restore a subreddit to its previous state."""
+    subreddit = reddit.subreddit(name)
+    subreddit.mod().update({
+        "public_description": description,
+        "subreddit_type": type
+    })
+
+def restore_contributor(name, user):
+    """Restore a user to a subreddit's approved submitters list."""
+    subreddit = reddit.subreddit(name)
+    subreddit.contributor.add(user)
+
+def private_subreddit(subreddit):
+    """Set a subreddit to private."""
+    # get the current description and type of the subreddit
+    old_description = subreddit.public_description
+    old_type = subreddit.mod().settings().get("subreddit_type")
+    
+    # remove all approved users
+    for contributor in subreddit.contributor():
+        subreddit.contributor.remove(contributor)
+
+    # new description can be set via the environment, or will default to a message about the blackout with relevant links
+    new_description = f"/r/{subreddit.display_name} {environ.get('SUBREDDIT_DESCRIPTION')}" or f"/r/{subreddit.display_name} is now private to protest Reddits API pricing changes and the death of 3rd party apps. Click here to find out why we have gone dark](https://www.theverge.com/2023/6/5/23749188/reddit-subreddit-private-protest-api-changes-apollo-charges). [You can also find a list of other subs that are going dark here](https://reddark.untone.uk/)."
+
+    # set the subreddit to private    
+    subreddit.mod.update({
+        "public_description": new_description,
+        "subreddit_type": "private",
+        "disable_contributor_requests": True
+    })
+
+    # return the old description and type so they can be restored later
+    return old_description, old_type
+
+# a function just as good practice - so this can be imported and called from other scripts
+def main():
+    """Set moderated subreddits to private."""
+    
+    # set the mode - if we have a backup file, restore subreddits. Otherwise, private them.
+    restore = exists("subreddits.csv") and exists("approved_users.csv")
+
+    if restore:
+        print("Restoring subreddits")
+        # count the number of subreddits and users restored
+        sub_count = 0
+        user_count = 0
+        with open("subreddits.csv", "r") as subreddits:
+            reader = csv.reader(subreddits)
+            for row in reader:
+                sub_count += 1
+                restore_subreddit(*row)
+        with open("approved_users.csv", "r") as approved_users:
+            reader = csv.reader(approved_users)
+            for row in reader:
+                user_count += 1
+                restore_contributor(*row)
+        print(f"Restored {sub_count} subreddits and {user_count} approved users")
+    else:
+        print("Privating subreddits")
+        # count the number of subreddits privated
+        sub_count = 0
+        with open("subreddits.csv", "w") as subreddits:
+            with open("approved_users.csv", "w") as approved_users:
+                subreddits_writer = csv.writer(subreddits)
+                approved_users_writer = csv.writer(approved_users)
+                for subreddit in selected_subreddits():
+                    sub_count += 1
+                    # backup approved users
+                    for contributor in subreddit.contributor():
+                        approved_users_writer.writerow([subreddit.display_name, contributor.name])
+                    
+                    # set subreddit to private
+                    old_description, old_type = private_subreddit(subreddit)
+
+                    # backup old description and type
+                    subreddits_writer.writerow([subreddit.display_name, old_description, old_type])
+                    print(f"Privated {subreddit.display_name}")
+        print(f"Privated {sub_count} subreddits")
+
+# runs the main function if the script is called directly (eg. `python3 wipe_reddit_account.py`)
+# without this the main method wouldn't run
+if __name__ == "__main__":
+    main()
